69735f9fc8105d86c37c7bf42d2c1b6b598916a4
Gunaseelan Rajamanickam (MS/ESV31-CC) <ugn1cob@bosch.com>
2022-02-08 15:34:25 +0530

    First commit

--- check_limits.py
+++ check_limits.py
@@ -1,18 +1,25 @@
 
-def battery_is_ok(temperature, soc, charge_rate):
-  if temperature < 0 or temperature > 45:
-    print('Temperature is out of range!')
-    return False
-  elif soc < 20 or soc > 80:
-    print('State of Charge is out of range!')
-    return False
-  elif charge_rate > 0.8:
-    print('Charge rate is out of range!')
-    return False
+battery_parameter_range = {
+  'temperature': {'min': 0, 'max': 45},
+  'soc': {'min': 20, 'max': 80},
+  'charge_rate': {'min': 0, 'max': 0.8}
+}
 
+def check_battery_parameter(battery_parameter, battery_parameter_value):
+  if battery_parameter_value < battery_parameter_range[battery_parameter]['min'] or battery_parameter_value > battery_parameter_range[battery_parameter]['max']:
+    print(battery_parameter + ' is out of range!')
+    return False
+  return True
+
+def battery_is_ok(battery_parameter):
+  parameter_verdict = {}
+  for attribute in battery_parameter:
+    parameter_verdict[attribute] = check_battery_parameter(attribute, battery_parameter[attribute])
+  if False in [parameter_verdict['temperature'], parameter_verdict['soc'], parameter_verdict['charge_rate']]:
+    return False
   return True
 
 
 if __name__ == '__main__':
-  assert(battery_is_ok(25, 70, 0.7) is True)
-  assert(battery_is_ok(50, 85, 0) is False)
+  assert(battery_is_ok({'temperature': 25, 'soc': 70, 'charge_rate': 0.7}) is True)
+  assert(battery_is_ok({'temperature': 50, 'soc': 85, 'charge_rate': 0}) is False)
